5eb907b2856469459c48535009c84e3a8183ae52
adam-mcdaniel <adam.mcdaniel17@gmail.com>
2019-09-17 19:46:42 -0400

    Implemented pest parser

--- src/compile.rs
+++ src/compile.rs
@@ -1,9 +1,9 @@
-use crate::{program, Compile, Target};
+use crate::{parser::parse, Compile, Target};
 
 use alloc::string::String;
 
 pub fn compile<T: Target>(script: &str) -> Result<String, String> {
-    match program().parse(script) {
+    match parse(script) {
         Ok(ast) => match Compile::<T>::compile(ast) {
             Ok(code_gen) => Ok(code_gen.replace(";", ";\n\t")),
             Err(e) => Err(format!("{:?}", e)),
